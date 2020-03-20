b38af2e970f00d108c220ddebbce5a4a63a237fd
Liliya Mykhailova <lm@codemotion.eu>
2020-03-20 20:39:20 +0200

    test(Hybris, SFCC): extended Hybris, SFCC tests

--- src/backends/__tests__/Hybris.spec.js
+++ src/backends/__tests__/Hybris.spec.js
@@ -1,16 +1,17 @@
-import { Hybris } from '../Hybris';
-import { ProductService } from '@amplience/sap-product-browser';
+import {Hybris} from '../Hybris';
+import {ProductService} from '@amplience/sap-product-browser';
+import {ProductSelectorError} from "../../ProductSelectorError";
 
 describe('Hybris', () => {
   it('should create an instance of ProductService', () => {
-    const params = { hybrisUrl: 'http://test.com' };
+    const params = {hybrisUrl: 'http://test.com'};
     const hybris = new Hybris(params);
 
     expect(hybris.productService).toEqual(new ProductService(params.hybrisUrl, '/rest/v2', null));
   });
 
   it('getItems should request product for each id', async () => {
-    const params = { hybrisUrl: 'http://test.com' };
+    const params = {hybrisUrl: 'http://test.com'};
     const hybris = new Hybris(params);
 
     let item = 0;
@@ -21,7 +22,7 @@ describe('Hybris', () => {
       return Promise.resolve({
         code: `${item}`,
         name: `<a>hello ${item}</a>`,
-        images: [{ format: 'product', url: `/cat-${item}` }],
+        images: [{format: 'product', url: `/cat-${item}`}],
         catalog: 'spa'
       });
     });
@@ -40,20 +41,43 @@ describe('Hybris', () => {
     expect(hybris.productService.getByCode).toHaveBeenCalledWith('another', '2', 'USD');
     expect(hybris.productService.getByCode).toHaveBeenCalledWith('spa2', '3', 'USD');
     expect(result).toEqual([
-      { id: '1', name: 'hello 1', image: 'http://test.com/cat-1', catalog: 'spa' },
-      { id: '2', name: 'hello 2', image: 'http://test.com/cat-2', catalog: 'another' },
-      { id: '3', name: 'hello 3', image: 'http://test.com/cat-3', catalog: 'spa2' }
+      {id: '1', name: 'hello 1', image: 'http://test.com/cat-1', catalog: 'spa'},
+      {id: '2', name: 'hello 2', image: 'http://test.com/cat-2', catalog: 'another'},
+      {id: '3', name: 'hello 3', image: 'http://test.com/cat-3', catalog: 'spa2'}
     ]);
   });
 
+  it('getItems should fail', async () => {
+    const params = {hybrisUrl: 'http://test.com'};
+    const hybris = new Hybris(params);
+
+    jest.spyOn(global, 'fetch').mockImplementation(() => Promise.reject('Unable to connect'));
+    jest.spyOn(console, 'error').mockImplementation(() => {
+    });
+
+    const state = {
+      params: {
+        currency: 'USD'
+      }
+    };
+    const ids = [{id: '1', catalog: 'spa'}, {id: '2', catalog: 'another'}, {id: '3', catalog: 'spa2'}];
+
+    try {
+     await hybris.getItems(state, ids);
+    } catch (e) {
+      expect(console.error).toBeCalledTimes(0);
+      expect(e).toEqual('Unable to connect');
+    }
+  });
+
   it('search should return pages and items', async () => {
-    const params = { hybrisUrl: 'http://test.com' };
+    const params = {hybrisUrl: 'http://test.com'};
     const hybris = new Hybris(params);
 
     jest.spyOn(hybris.productService, 'search').mockImplementation(() => {
       return Promise.resolve({
         products: [
-          { code: '1', name: '<span>hello</span>', images: null, defaultImageUrl: '/cat' }
+          {code: '1', name: '<span>hello</span>', images: null, defaultImageUrl: '/cat'}
         ],
         pagination: {
           totalResults: 10,
@@ -66,7 +90,7 @@ describe('Hybris', () => {
     const state = {
       selectedCatalog: 'spa',
       searchText: 'hello',
-      page: { curPage: 0 },
+      page: {curPage: 0},
       params: {
         currency: 'USD'
       }
@@ -76,12 +100,27 @@ describe('Hybris', () => {
 
     expect(hybris.productService.search).toBeCalledWith('spa', 'hello', 'USD', 0);
     expect(result).toEqual({
-      items: [{ id: '1', name: 'hello', image: '/cat', catalog: 'spa' }],
+      items: [{id: '1', name: 'hello', image: '/cat', catalog: 'spa'}],
       page: {
         total: 10,
         curPage: 0,
         numPages: 2
       }
-    }) 
+    })
+  });
+
+  it('should export item', async () => {
+    const params = {hybrisUrl: 'http://test.com'};
+    const hybris = new Hybris(params);
+
+    const result = hybris.exportItem({
+      id: '1',
+      catalog: 'amp'
+    });
+
+    expect(result).toEqual({
+      id: '1',
+      catalog: 'amp'
+    });
   });
 });
